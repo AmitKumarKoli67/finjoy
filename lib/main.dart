361b5738436a789a3196d95d991c1e950d1339e5
Amitkumarkoli67 <amitkkoli67@gmail.com>
2026-05-07 21:57:10 +0530

    feat: hive setup and bloc provider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:finjoy/features/splash/screens/splash_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'data/models/transaction_model.dart';
+import 'data/repositories/transaction_repository.dart';
+import 'features/splash/screens/splash_screen.dart';
+import 'features/transactions/cubit/transaction_cubit.dart';
 
- void main(){
-  runApp(FinjoyApp());
- }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(TransactionModelAdapter());
+  await Hive.openBox<TransactionModel>('transactions');
+  runApp(const FinjoyApp());
+}
 
-class FinjoyApp extends StatelessWidget{
+class FinjoyApp extends StatelessWidget {
   const FinjoyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Finjoy',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: SplashScreen()
+    return BlocProvider(
+      create: (_) => TransactionCubit(TransactionRepository()),
+      child: MaterialApp(
+        title: 'Finjoy',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        home: const SplashScreen(),
+      ),
     );
   }
 }
